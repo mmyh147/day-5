84bb76a2c47b66646d67747fed589357098d6f30
mmyh147 <61750916+mmyh147@users.noreply.github.com>
2024-02-15 15:00:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,111 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+
+
+    //    ============== task 1 ================
+
+//        int [] num = {50, -20, 0, 30, 40, 60, 10};
+//        System.out.println(Arrays.toString(num));
+//        System.out.println(num[0] == num[num.length - 1]);
+
+        //    ============== Task 2  ================
+
+//        int [] num = {1,4,17,7,25,3,100};
+//        int sum = 0, avg = 0;
+//
+//        for (int n : num){
+//            sum += n;
+//        }
+//
+//        avg = sum / num.length;
+//        System.out.println("The average is: " + avg);
+//        System.out.print("the numbers that are greater than the average are: ");
+//        for (int n : num){
+//            if(n > avg){
+//                System.out.print(" " + n);
+//            }
+//        }
+
+        //    ============== Task 3  ================
+
+//        int [] num = {20,30,40};
+//
+//        if(num[0] > num[num.length - 1]){
+//
+//            System.out.println("The larger value between first and last element: " + num[0]);
+//        }else if(num[0] < num[num.length - 1]){
+//            System.out.println("The larger value between first and last element: " + num[num.length - 1]);
+//
+//        }else {
+//            System.out.println("both are equal: " + num[0]);
+//        }
+
+        //    ============== Task 4  ================
+
+//        int[] num = {20,30,40};
+//        int[] swap = Arrays.copyOf(num, num.length);
+//
+//        swap[0] = num[num.length - 1];
+//        swap[swap.length - 1] = num[0];
+//
+//        System.out.println("Original array: " + Arrays.toString(num));
+//        System.out.println("Swap array: " + Arrays.toString(swap));
+
+
+        //    ============== Task 5  ================
+
+//
+//                int[] num = {2, 3, 40, 1, 5, 9, 4, 10, 7};
+//
+//                System.out.println("Original Array: " + Arrays.toString(num));
+//
+//                int[] temp = new int[num.length];
+//                int Index = 0;
+//
+//                for (int n: num) {
+//                    if (n % 2 != 0) {
+//                        temp[Index++] = n;
+//                    }
+//                }
+//
+//                for (int n: num) {
+//                    if (n % 2 == 0) {
+//                        temp[Index++] = n;
+//                    }
+//                }
+//                num = Arrays.copyOf(temp, temp.length);
+//                System.out.println("new Array: " + Arrays.toString(num));
+
+
+
+        //    ============== Task 6  ================
+
+//                int[] array1 = {2, 3, 6, 6, 4};
+//                int[] array2 = {2, 3, 6, 6, 4};
+//
+//                System.out.println("Array 1: " + Arrays.toString(array1));
+//                System.out.println("Array 2: " + Arrays.toString(array2));
+//
+//                System.out.println("equal: " + Arrays.equals(array1, array2));
+
+
+
+
+
+
+
+
+    }
+
+    }
